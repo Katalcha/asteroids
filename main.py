ce2335d0ff8c9afe15706b48124271ef8977d5f5
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-10-04 16:34:29 +0200

    add deltatime and limit fps to 60

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
-
-    while(True):
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        # limits framerate to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
