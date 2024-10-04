ba5a2290dcf0edbbf0166952651acdbff4679e7f
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-10-04 17:26:19 +0200

    add pygame.sprite.Groups
    organize sprites in 'updateable' and 'drawable'
    
    add static field 'containers' to player class

--- main.py
+++ main.py
@@ -6,17 +6,27 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for sprite in updateable:
+            sprite.update(dt)
+
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
 
         # limits framerate to 60
